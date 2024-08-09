0988697aa046861bb1110b748f1127109895514b
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-09 14:48:35 +0200

    User form validations

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -8,6 +8,7 @@ import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
+import { UserFormValidation } from "@/libs/validation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -19,24 +20,20 @@ export enum FormFieldType {
     SKELETON = 'skeleton',
 }
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
-
 const PatientForm = () => {
     const [isLoading, setIsLoading] = useState(false);
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
             defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: "",
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof UserFormValidation>) {
         setIsLoading(true);
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
