c1342d83767d97127873937fe5e8bd22d27e55a6
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-09 12:51:06 +0200

    Design button submit

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,6 +6,8 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -24,6 +26,7 @@ const formSchema = z.object({
 })
 
 const PatientForm = () => {
+    const [isLoading, setIsLoading] = useState(false);
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -34,9 +37,12 @@ const PatientForm = () => {
 
     // 2. Define a submit handler.
     function onSubmit(values: z.infer<typeof formSchema>) {
+        setIsLoading(true);
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
+
+        setIsLoading(false);
     }
     return (
         <Form {...form}>
@@ -74,7 +80,7 @@ const PatientForm = () => {
                     placeholder="250788888888"
                 />
 
-                <Button type="submit">Submit</Button>
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
     )
