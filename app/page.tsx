bd4679c29cfe08331b992be3727ee7cb78db6cc5
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-09 12:45:11 +0200

    Design patient form fields

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import PatientForm from '@/components/forms/PatientForm'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -15,7 +16,7 @@ export default function Home() {
                         className="mb-12 h-10 w-fit"
                     />
 
-                    Patient Form
+                    <PatientForm />
 
                     <div className="text-14-regular mt-20 flex justify-between">
                         <p className="justify-items-end text-dark-600 xl:text-left">
